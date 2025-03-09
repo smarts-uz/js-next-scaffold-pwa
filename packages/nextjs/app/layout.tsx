c776dc2e27da2f6cf02f240d076c0b2a1f977488
Ramz001 <rkenjaev1@gmail.com>
2025-03-09 18:24:29 +0500

    work on fixing fullscreen bug

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -13,10 +13,13 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
       <head>
-        <meta name="viewport" content="width=device-width; initial-scale=1; viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/android-chrome-192x192.png" />
       </head>
       <body>
         <ThemeProvider enableSystem>
